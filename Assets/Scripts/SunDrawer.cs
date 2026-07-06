22200e3df36700f4caadf3f451cc8946c5d10e4c
chebuo <bob.chebuo@gmail.com>
2026-07-06 13:07:05 +0900

    [fix] 重さ対策+nullチェック

--- Assets/Scripts/SunDrawer.cs
+++ Assets/Scripts/SunDrawer.cs
@@ -5,11 +5,17 @@ public class SunDrawer : MonoBehaviour
     public Light sun;
     public float distance = 1000f;
 
+    Camera mainCamera;
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
     void Update()
     {
+        if(sun == null) return;
         Vector3 dir = -sun.transform.forward;
-        transform.position = Camera.main.transform.position + dir * distance;
+        transform.position = mainCamera.transform.position + dir * distance;
 
-        transform.LookAt(Camera.main.transform);
+        transform.LookAt(mainCamera.transform);
     }
 }
