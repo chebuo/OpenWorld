20c01b3e652e2db3845ff643815e3e19fe56dec0
chebuo <bob.chebuo@gmail.com>
2026-07-11 00:56:31 +0900

    [add] playerの動かす処理

--- Assets/DoodleWorld/Scripts/PlayerController.cs
+++ Assets/DoodleWorld/Scripts/PlayerController.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] float attackForce = 5f;
+
+    InputAction move;
+    InputAction jump;
+    InputAction attack;
+
+    Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        move=InputSystem.actions.FindAction("Move");
+        jump=InputSystem.actions.FindAction("Jump");
+        attack=InputSystem.actions.FindAction("Attack");
+        rb=this.GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move()
     {
-        
+        var moveValue = move.ReadValue<Vector2>();
+        rb.AddForce(new Vector3(moveValue.x * moveSpeed, 0, moveValue.y * moveSpeed));
+        Debug.Log(rb.linearVelocity);
+    }
+
+    public void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    public void Attack()
+    {
+        // Implementation for attack logic
     }
 }
