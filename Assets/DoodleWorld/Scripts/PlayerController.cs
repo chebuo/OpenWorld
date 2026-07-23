a3a9b5d5921f7891d20882796d820fcb3e8b2109
chebuo <bob.chebuo@gmail.com>
2026-07-24 01:06:36 +0900

    [add] 視点の角度変更機能

--- Assets/DoodleWorld/Scripts/PlayerController.cs
+++ Assets/DoodleWorld/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     InputAction move;
     InputAction jump;
     InputAction attack;
+    InputAction look;
 
     Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,15 +19,30 @@ public class PlayerController : MonoBehaviour
         move=InputSystem.actions.FindAction("Move");
         jump=InputSystem.actions.FindAction("Jump");
         attack=InputSystem.actions.FindAction("Attack");
+        look=InputSystem.actions.FindAction("Look");
+        move.Enable();
+        jump.Enable();
+        attack.Enable();
+        look.Enable();
         rb=this.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        ChangeDir();
     }
 
     public void Move()
     {
         var moveValue = move.ReadValue<Vector2>();
-        rb.AddForce(new Vector3(moveValue.x * moveSpeed, 0, moveValue.y * moveSpeed));
-        Debug.Log(rb.linearVelocity);
+        Vector3 moveDir =
+        transform.forward * moveValue.y +
+        transform.right * moveValue.x;
+
+        rb.linearVelocity = new Vector3(moveDir.x * moveSpeed,rb.linearVelocity.y,moveDir.z * moveSpeed);
     }
+
+
 
     public void Jump()
     {
@@ -36,5 +52,11 @@ public class PlayerController : MonoBehaviour
     public void Attack()
     {
         // Implementation for attack logic
+    }
+
+    public void ChangeDir()
+    {
+        var lookValue=look.ReadValue<Vector2>();
+        transform.rotation=Quaternion.Euler(0,lookValue.x,0);
     }
 }
