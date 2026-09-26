c65a0b3afe8f89ca91ba01908d3cd2744a475cb0
chebuo <bob.chebuo@gmail.com>
2026-09-27 01:09:50 +0900

    [add] 所有した時のアイテム情報を保持するためのクラス作成

--- Assets/DoodleWorld/Data/ItemData.cs
+++ Assets/DoodleWorld/Data/ItemData.cs
@@ -3,12 +3,24 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ItemData", menuName = "Scriptable Objects/ItemData")]
 public class ItemData : ScriptableObject
 {
-    public ItemParam[] playerParams;
-    [System.Serializable]
-    public class ItemParam
-    {
-        public float hardness;
-        public float power;
-        public float rarity;
-    }
+    public ItemParams[] items;
+}
+
+[System.Serializable]
+public class ItemParams
+{
+    public ItemProp itemProp;
+    public GameObject prefab;
+
+    public int maxHP;
+    public float hardness;
+    public float rarity;
+}
+
+[System.Serializable]
+public class ItemProp
+{
+    public GameObject shape;
+    public float power;
+    public float size;
 }
